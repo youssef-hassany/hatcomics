fc34bc5ad2e12e297743365e64f730747d7fd7ee
Youssef Hassany <youssefhassany22@gmail.com>
2025-08-09 02:39:50 +0300

    ordering top comics bug

--- app/api/comics/top/route.ts
+++ app/api/comics/top/route.ts
@@ -10,7 +10,7 @@ export async function GET() {
 
     const topComics = await prisma.comic.findMany({
       orderBy: {
-        averageRating: "desc",
+        averageRating: "asc",
       },
       take: 5,
     });
