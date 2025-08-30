275a5e91805b562486aa617238fe3c90cf6f7da7
Youssef Hassany <youssefhassany22@gmail.com>
2025-08-31 01:41:47 +0300

    disable middleware for adsense

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+/*
 import { clerkMiddleware } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -79,3 +80,4 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
   ],
 };
+*/
