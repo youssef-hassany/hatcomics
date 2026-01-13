a32a23a45d5af06fa550ed2ad1021fa123575bf3
Youssef Hassany <youssefhassany22@gmail.com>
2026-01-13 02:23:51 +0200

    fix pfp url's not found issue

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,27 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "img.clerk.com",
-      "comicvine.gamespot.com",
-      "pub-f34b4d53fd6f497180254197682d3a25.r2.dev",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "*.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "pub-f34b4d53fd6f497180254197682d3a25.r2.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "comicvine.gamespot.com",
+      },
     ],
   },
 };
